cee61c089c13c495eee34441a924627afebf4249
Myakinina Yuliya <myakinina_yu@mail.ru>
2023-01-11 19:57:02 +0300

    07. Что такое компонент?

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
+      <div><Header/></div>
       <ul>
         <li>html</li>
         <li>css</li>
         <li>js</li>
         <li>react</li>
       </ul>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div className="App">
+        <a href='#s'>Home</a>
+        <a href='#s'>News Feed</a>
+        <a href='#s'>Messages</a>
     </div>
   );
 }
