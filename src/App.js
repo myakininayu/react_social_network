2952c216247f02f9354a257f9e93a2d7c8d9f26e
Myakinina Yuliya <myakinina_yu@mail.ru>
2023-01-13 12:37:38 +0300

    09. Import/Export

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import './App.css';
+import './App.css'
+import Header from './Header';
+import Technologies from './Technologies';
+
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
       <Technologies/>
-    </div>
-  );
-}
-
-const Header = () => {
-  return (
-    <div className="App">
-        <a href='#s'>Home</a>
-        <a href='#s'>News Feed</a>
-        <a href='#s'>Messages</a>
-    </div>
-  );
-}
-
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
     </div>
   );
 }
